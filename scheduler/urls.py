2672ac84e9ab9b36c2523ea53889876d7a45bbe7
Fayiz kv <fayizkvcklm2002@gmail.com>
2024-11-22 20:47:13 +0530

    all cleared

--- scheduler/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('availability/', views.availability_register, name='availability'),
-    path('hr/', views.hr_dashboard, name='hr'),
-]
